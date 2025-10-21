4831308815d97ae5209a34a8582339e244a52b92
João <moreirajoaopaulo951@gmail.com>
2025-10-21 09:49:26 -0300

    carrosel finalizado

--- src/app/carrosel/carrosel.ts
+++ src/app/carrosel/carrosel.ts
@@ -1,5 +1,5 @@
 import { NgClass } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-carrosel',
@@ -8,38 +8,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carrosel.scss']
 
 })
-export class Carrosel implements  OnInit {
-
-  visivel :boolean = false;
-  contador : number = 0;
-  direcao : number= 0;
-  carrosel:  string[] = ['imagem01.png', 'imagem02.png', 'imagem03.png'];
-
-  ngOnInit(): void {
-      setTimeout(() => {
-        this.visivel = true;
-      }, 1000)
-  }
-
-  trocaProxima(){
-    setTimeout(()=> {
-    this.contador++;
-     if(this.contador > 2){
-        this.contador = 0;
-     }
-    }, 200);
-  }
-
-  trocaAnterior(){
-    setTimeout(()=> {
-    this.contador--;
-        if(this.contador < 0){
-          this.contador = 2;
-        }
-    }, 200);
-  }
-
-  
-
+export class Carrosel {
 
 }
